1b3ce595a8a04f166a6a5e55612e0bdfe851818a
mynameisted <contact.tedlai@gmail.com>
2018-10-05 01:41:14 +0800

    Commit of function 4 - Brandon

--- python/gebiz/function4.py
+++ python/gebiz/function4.py
@@ -1,50 +1,48 @@
 import csv
-
-#======================POINTS TO NOTE======================
-# masterGebiz will be a list of dictionaries, where each dictionary is every row in the gebiz CSV file.
-# masterCon will be a list of dictionairies, where each dictionary is every row in the registered contractor CSV file.
-# regCon will be a list of unique company names found in the registered contractors. Essentially, a list of registered contractors.
-# You'll have to ensure that the regCon list is unique (no duplicated company names)
-#When you run this file, nothing will be shown in the console/terminal. You'll have to print certain values if necessary for troubleshooting/development
-
-def sortRegistered(gebizpath, regconpath):
-    #Create empty lists
-    masterGebiz = []
-    masterCon = []
-    regCon = []
-    #Open Registered Contractors CSV file
-    with open(regconpath) as oFile:
-        reader = csv.DictReader(oFile)
-        #Loop through every row in the file
-        for row in reader:
-            #Replace 'company_name' data with normalised data (Uppercase, remove fullstops and double spaces)
-            row['company_name'] = row['company_name'].replace(".","").replace("  "," ").upper();
-            #Append the company name into regCon (This list only holds the contractor names)
-            regCon.append(row['company_name'])
-            #Append the full row of data into masterCon
-            masterCon.append(row)
+'''(get master contract data AND registered contract data first), get master award data'''
 
 
-    with open(gebizpath) as gFile:
-        #Open geBiz CSV file
-        reader = csv.DictReader(gFile)
-        #Loop through every row in the file
-        for row in reader:
-            #Replace 'supplier_name' data with normalised data (Uppercase, remove fullstops and double spaces)
-            row['supplier_name'] = row['supplier_name'].replace(".","").replace("  "," ").upper();
-            #Check if current row's supplier_name is found in the list of registered contractors
-            if row['supplier_name'] in regCon:
-                #Put whatever code you want to execute
-                #print "Supplier is a registered contractor"
-                pass
-            #Else if current row's supplier name is not found in the list of registered contractors (meaning unregistered)
-            else:
-                #Put whatever code you want to execute
-                #print "Supplier is not a registered contractor"
-                pass
-            #Append the full row of data into masterGebiz
-            masterGebiz.append(row)
+def get_contractor_data(regconfilepath):
+    mastercontdata = []  # blank list to store dicts of master contractor data
+    with open('regconfilepath','rU') as f:  # read one line at a time from registered-contractors.csv
+        reader = csv.DictReader(f)  # dictionary initialized
+        for row in reader:  # for every row in reader
+            row['company_name'] = row['company_name'].replace('.', '').replace('  ', ' ').upper()
+            mastercontdata.append(row)
+    return mastercontdata
 
 
-#Call the function to execute with selected file paths (Will vary depending on your folder structure)
-sortRegistered("government-procurement/government-procurement-via-gebiz.csv","listing-of-registered-contractors/listing-of-registered-contractors.csv")
+def get_award_data(awardfilepath):
+    with open('awardfilepath', 'rU') as f:  # read one line at a time from awarded-contractors.csv
+        reader = csv.DictReader(f)  # dictionary initialized
+        masterawardData = [] # blank list to store master award data
+        i = 1
+        for row in reader:  # for every row in reader
+            row['supplier_name'] = row['supplier_name'].replace('.','').replace('  ',' ').upper()
+            if row['supplier_name'] in regcontdata:
+                masterawardData.append(row)
+    return masterawardData
+
+
+def splitconts(masterawardData, mastercontdata):
+        listAwdRegConts = []  # list for awarded registered contractors
+        listNotRegConts = []  # list for non registered contractors
+        for i in range(len(mastercontdata)):  # append listAwdRegConts with contractor names found in both lists
+            if mastercontdata[i]['company_name'] in masterawardData:
+                if mastercontdata[i]['company_name'] not in listAwdRegConts:
+                    listAwdRegConts.append(mastercontdata[i]['company_name'])
+                else: # company name already exists in listAwdRegConts
+                    continue
+            else:  # append listNotRegConts if contractor name not found in both lists
+                listNotRegConts.append(mastercontdata[i]['company_name'])
+        return listAwdRegConts, listNotRegConts
+
+
+def listawardedregisteredcontractors(listAwdRegConts):
+        if len(listAwdRegConts) == 0:  # no awarded registered contractors
+            print "There are no awarded registered contractors."
+        else:  # print out all awarded registered contractors in order
+            print "Names of awarded registered contractors:"
+            for i in range(len(listAwdRegConts)):
+                print "%d." % (i+1) + listAwdRegConts[i]
+        print "No. of awarded registered contractors: %d" % len(listAwdRegConts)
