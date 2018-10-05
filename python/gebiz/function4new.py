e30e255dccde066c8c7198b6d711cb62cf6e14ef
mynameisted <contact.tedlai@gmail.com>
2018-10-05 19:52:37 +0800

    Updated function4new to final status. Commit of data for registered and unregistered suppliers' gebiz awards.

--- python/gebiz/function4new.py
+++ python/gebiz/function4new.py
@@ -1,19 +1,22 @@
 import csv
-
 #======================POINTS TO NOTE======================
 # masterGebiz will be a list of dictionaries, where each dictionary is every row in the gebiz CSV file.
 # masterCon will be a list of dictionairies, where each dictionary is every row in the registered contractor CSV file.
-# regCon will be a list of unique company names found in the registered contractors. Essentially, a list of registered contractors.
-# You'll have to ensure that the regCon list is unique (no duplicated company names)
-#When you run this file, nothing will be shown in the console/terminal. You'll have to print certain values if necessary for troubleshooting/development
+# regConNames will be a list of unique company names found in the registered contractors. Essentially, a list of registered contractors.
+# regConNamesAwarded is a list of all awarded registered company names 
+# unregConNamesAwarded is a list of all awarded unregistered company names 
+# gebizRegisteredAwards is a list of all awarded details for registered companies.
+# gebizUnregisteredAwards is a list of all awarded details for unregistered companies.
 
 def sortRegistered(gebizpath, regconpath):
     #Create empty lists
     masterGebiz = []
     masterCon = []
-    regCon = []
-    regConAwarded = []
-    unregConAwarded = []
+    regConNames = []
+    regConNamesAwarded = []
+    unregConNamesAwarded = []
+    gebizRegisteredAwards = []
+    gebizUnregisteredAwards = []
     #Open Registered Contractors CSV file
     with open(regconpath) as oFile:
         reader = csv.DictReader(oFile)
@@ -21,9 +24,9 @@ def sortRegistered(gebizpath, regconpath):
         for row in reader:
             #Replace 'company_name' data with normalised data (Uppercase, remove fullstops and double spaces)
             row['company_name'] = row['company_name'].replace(".","").replace("  "," ").upper();
-            #Append the company name into regCon (This list only holds the contractor names)
-            if row['company_name'] not in regCon:
-                regCon.append(row['company_name'])
+            #Append the company name into regConNames (This list only holds the contractor names)
+            if row['company_name'] not in regConNames:
+                regConNames.append(row['company_name'])
             #Append the full row of data into masterCon
             masterCon.append(row)
 
@@ -35,19 +38,21 @@ def sortRegistered(gebizpath, regconpath):
             #Replace 'supplier_name' data with normalised data (Uppercase, remove fullstops and double spaces)
             row['supplier_name'] = row['supplier_name'].replace(".","").replace("  "," ").upper();
             #Check if current row's supplier_name is found in the list of registered contractors
-            if row['supplier_name'] in regCon:
-                #Put whatever code you want to execute
-                #print "Supplier is a registered contractor"
-                if row['supplier_name'] not in regConAwarded:
-                    regConAwarded.append(row['supplier_name'])
+            if row['supplier_name'] in regConNames:
+                #Append the row into a consolidated list    
+                gebizRegisteredAwards.append(row)
+                #Ensure no duplicates in the list of names
+                if row['supplier_name'] not in regConNamesAwarded:
+                    regConNamesAwarded.append(row['supplier_name'])
             #Else if current row's supplier name is not found in the list of registered contractors (meaning unregistered)
             else:
-                #Put whatever code you want to execute
-                #print "Supplier is not a registered contractor"
-                if row['supplier_name'] not in unregConAwarded:
-                    unregConAwarded.append(row['supplier_name'])
+                if row['supplier_name'] != 'NA':
+                    #Append the row into a consolidated list
+                    gebizUnregisteredAwards.append(row)
+                    #Ensure no duplicates in the list of names
+                    if row['supplier_name'] not in unregConNamesAwarded:
+                        unregConNamesAwarded.append(row['supplier_name'])
             #Append the full row of data into masterGebiz
             masterGebiz.append(row)
-    return {'gebizData':masterGebiz,'regConData':masterCon,'regCon':regCon,'regConAwarded':regConAwarded,'unregConAwarded':unregConAwarded}
-#Call the function to execute with selected file paths (Will vary depending on your folder structure)
-#sortRegistered("government-procurement/government-procurement-via-gebiz.csv","listing-of-registered-contractors/listing-of-registered-contractors.csv")
+    return {'masterGebiz':masterGebiz,'masterCon':masterCon,'regConNames':regConNames,'regConNamesAwarded':regConNamesAwarded,'unregConNamesAwarded':unregConNamesAwarded,'gebizRegisteredAwards':gebizRegisteredAwards,'gebizUnregisteredAwards':gebizUnregisteredAwards}
+#data = sortRegistered("government-procurement/government-procurement-via-gebiz.csv","listing-of-registered-contractors/listing-of-registered-contractors.csv")
